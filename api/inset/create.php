e1a210b0231d7ead31e65c6f00040cc20c16f0e8
Radoslaw Tomaszewski <poczta@radtomas.pl>
2020-02-17 21:54:53 +0100

    Add create inset endpoint and update database create sql file.

---
+++ api/inset/create.php
@@ -0,0 +1,33 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-Width");
+
+include_once '../config/database.php';
+include_once '../objects/inset.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$inset = new Inset($db);
+
+$data = json_decode(file_get_contents("php://input"));
+if (!empty($data->title) && !empty($data->content)) {
+  $inset->title = $data->title;
+  $inset->content = $data->content;
+
+  if ($inset->create()) {
+    http_response_code(201);
+    echo json_encode(array("message" => "Inset was created."));
+  } else {
+    http_response_code(503);
+    echo json_encode(array("message" => "Unable to create Inset."));
+  }
+} else {
+  http_response_code(400);
+  echo json_encode(array("message" => "Unable to create Inset. Data is incomplete."))
+}
+?>
--- api/objects/inset.php
+++ api/objects/inset.php
@@ -18,5 +18,21 @@ class Inset {
     $stmt->execute();
     return $stmt;
   }
+
+  function create() {
+    $query = "INSERT INTO " . $this->table_name . "SET title=:title, content=:content";
+
+    $stmt = $this->conn->prepare($query);
+    $this->title = htmlspecialchars(strip_tags($this->name));
+    $this->content = htmlspecialchars(strip_tags($this->content));
+
+    $stmt->bindParam(":title", $this->title);
+    $stmt->bindParam(":content", $this->content);
+
+    if ($stmt->execute()) {
+      return true;
+    }
+    return false;
+  }
 }
 ?>
